f80ec81320ae2218bc4cfbcbe744bbb702643eef
ahmiura <39716937+ahmiura@users.noreply.github.com>
2018-09-15 17:54:55 -0300

    Update JenkinsFile_AngularCI.groovy

--- JenkinsFile_AngularCI.groovy
+++ JenkinsFile_AngularCI.groovy
@@ -5,8 +5,16 @@ pipeline {
 
     stages {
         stage('Checkout - Git') {
-            checkout scm
+            //checkout scm
+            steps {
+                git branch: 'release-candidate-1.0.0',
+                    credentialsId: 'Id_credential_jenkins',
+                    url: 'https://github.com/ahmiura/angular_alurapic.git'
+
+                sh "ls -lat"
+            }
         }
+        
         stage('NPM') {
             steps {
                 sh 'npm install '
