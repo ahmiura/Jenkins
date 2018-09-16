d47c6bb72e4d74925c7f7a1e5e5eaa8aed1bb3c1
Alex Miura <ahmiura@gmail.com>
2018-09-16 00:19:26 -0300

    criacao do path

--- JenkinsFile_AngularCI.groovy
+++ JenkinsFile_AngularCI.groovy
@@ -1,5 +1,12 @@
 #!groovy
 
+node {
+    env.NODEJS_HOME = "${tool 'Node 8.x'}"
+    // on linux / mac
+    env.PATH="${env.NODEJS_HOME}/bin:${env.PATH}"
+    sh 'npm --version'
+}
+
 pipeline {
     agent any
 
