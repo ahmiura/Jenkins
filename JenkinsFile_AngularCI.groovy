b15b324c2321b0458cd71244e86425357ebf8bcc
Alex Miura <ahmiura@gmail.com>
2018-09-16 22:48:53 -0300

    criacao do path

--- JenkinsFile_AngularCI.groovy
+++ JenkinsFile_AngularCI.groovy
@@ -6,8 +6,11 @@ pipeline {
     }
     stages {
         stage('Checkout - Git') {
-            //checkout scm
             steps {
+                checkout scm
+            }
+    
+            /*steps {
                 deleteDir()
 
                 git branch: 'release-candidate-1.0.0',
@@ -15,23 +18,18 @@ pipeline {
                     url: 'https://github.com/ahmiura/angular_alurapic.git'
 
                 sh "ls -lat"
-            }
+            }*/
         }
         
-        stage('NPM') {
+        stage('npm install') {
             steps {
                 sh 'npm install'
-                //nodejs(nodeJSInstallationName: 'Node 8.x', configId: '73c7e55b-1ed5-4573-88a1-da3f66448d73') {
-                //    sh 'npm install'
-                //}
-                
             }
         }
 
         stage('Test') {
             steps {
                 sh 'ng test --progress=false --watch false'
-
             }
                 
         }
